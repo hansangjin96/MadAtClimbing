a30ada36c5558aad323f6c6115828b8f483a0a2f
SangJin Han <s202188@naver.com>
2022-06-19 13:33:52 +0900

    Create Project

---
+++ MadAtClimbing/MadAtClimbing/MadAtClimbingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MadAtClimbingApp.swift
+//  MadAtClimbing
+//
+//  Created by 한상진 on 2022/06/19.
+//
+
+import SwiftUI
+
+@main
+struct MadAtClimbingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
